ae1da0c2aaf36a9607ae98c2778e41bdb51c251f
KemalArasTosunlar <tosunlarkemal@gmail.com>
2025-02-10 18:00:17 +0300

    created react project with vite, Installed all dependencies

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Basic configuration settings
+  server: {
+    port: 3000, // Default port for the Vite development server
+  },
+});
