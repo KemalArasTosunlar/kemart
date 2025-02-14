ef3a04890d2320e0d6a67cd25c0332cd6a6d4f3f
KemalArasTosunlar <tosunlarkemal@gmail.com>
2025-02-14 17:13:36 +0300

    feat: add new routes for Shop, About, Blog, and Contact pages in App and main

--- src/app.jsx
+++ src/app.jsx
@@ -4,12 +4,20 @@ import Header from './layout/Header';
 import HomePage from './pages/HomePage';
 import Footer from './layout/Footer';
 
+import ShopPageDesktop from './pages/ShopPageDesktop';
+import ContactPageDesktop from './pages/ContactPageDesktop';
+
 function App() {
   return (
     <>
       <Header /> {/* This will be shown on all pages */}
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="shop" element={<ShopPageDesktop />} />
+        <Route path="about" element={<div>About Page</div>} />
+        <Route path="blog" element={<div>Blog Page</div>} />
+        <Route path="contact" element={<ContactPageDesktop/>} />
+        
       </Routes>
       <Footer /> {/* This will be shown on all pages */}
     </>
