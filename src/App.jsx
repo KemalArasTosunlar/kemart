0936b70f71fb0a8602a3a41b4039ed7257203cf9
KemalArasTosunlar <tosunlarkemal@gmail.com>
2025-02-14 19:31:06 +0300

    Router deneme

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Header from "./layout/Header"
 import Footer from "./layout/Footer"
 import HomePage from "./pages/HomePage"
+import ShopPageDesktop from "./pages/ShopPageDesktop"
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="shop" element={<ShopPageDesktop/>} />
       </Routes>
       <Footer />
     </>
