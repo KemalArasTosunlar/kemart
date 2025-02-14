0bdc63146b607d57560e6e0b2b580cf0052bd8e2
KemalArasTosunlar <tosunlarkemal@gmail.com>
2025-02-14 16:57:06 +0300

    refactor: implement routing with BrowserRouter and add Header/Footer components in App

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,18 @@
-// create a fınctional component
 import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Header from './layout/Header';
+import HomePage from './pages/HomePage';
+import Footer from './layout/Footer';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <h1>App Component</h1>
-    </div>
+    <>
+      <Header /> {/* This will be shown on all pages */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+      <Footer /> {/* This will be shown on all pages */}
+    </>
   );
 }
 
