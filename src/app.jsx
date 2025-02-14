9a08d72c419ea096a5537228227dcf499fa9c0ac
KemalArasTosunlar <tosunlarkemal@gmail.com>
2025-02-14 18:04:20 +0300

    self-test-1

--- src/app.jsx
+++ src/app.jsx
@@ -3,15 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import Header from './layout/Header';
 
 import Footer from './layout/Footer';
+import HomePage from './pages/HomePage';
 
 
 function App() {
   return (
     <>
       <Header /> {/* This will be shown on all pages */}
+      
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
+      
+ 
       <Footer /> {/* This will be shown on all pages */}
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,14 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import App from "./App.jsx";  
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import "./index.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
+import * as React from "react"
+import * as ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import App from "./App"
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
+
