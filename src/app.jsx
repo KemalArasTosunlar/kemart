ff316058a3252687e05cac4783022e91f9fedbd1
KemalArasTosunlar <tosunlarkemal@gmail.com>
2025-02-14 17:22:12 +0300

    fix: update HomePage component reference to Home in App

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import Header from './layout/Header';
-import HomePage from './pages/HomePage';
+
 import Footer from './layout/Footer';
 
 
@@ -10,8 +10,7 @@ function App() {
     <>
       <Header /> {/* This will be shown on all pages */}
       <Routes>
-        <Route path="/" element={<HomePage />} />
-
+        <Route path="/" element={<Home />} />
       </Routes>
       <Footer /> {/* This will be shown on all pages */}
     </>
