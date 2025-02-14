d3e674ef9cc9c16cad4b000067d0157bb79e318b
KemalArasTosunlar <tosunlarkemal@gmail.com>
2025-02-14 17:18:07 +0300

    refactor: remove unused routes for Shop, About, Blog, and Contact pages in App

--- src/app.jsx
+++ src/app.jsx
@@ -4,8 +4,6 @@ import Header from './layout/Header';
 import HomePage from './pages/HomePage';
 import Footer from './layout/Footer';
 
-import ShopPageDesktop from './pages/ShopPageDesktop';
-import ContactPageDesktop from './pages/ContactPageDesktop';
 
 function App() {
   return (
@@ -13,11 +11,7 @@ function App() {
       <Header /> {/* This will be shown on all pages */}
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="shop" element={<ShopPageDesktop />} />
-        <Route path="about" element={<div>About Page</div>} />
-        <Route path="blog" element={<div>Blog Page</div>} />
-        <Route path="contact" element={<ContactPageDesktop/>} />
-        
+
       </Routes>
       <Footer /> {/* This will be shown on all pages */}
     </>
