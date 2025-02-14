66c71e36a578774046da49dbc8313d2196e78a58
KemalArasTosunlar <tosunlarkemal@gmail.com>
2025-02-14 15:53:52 +0300

    test 3

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
-import Header from './layout/Header.jsx';
-import Footer from './layout/Footer.jsx';
-import PageContent from './layout/PageContent.jsx';
+
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
-      <Header />
-      <PageContent />
-      <Footer />
+      Kemart
     </div>
   );
 }
