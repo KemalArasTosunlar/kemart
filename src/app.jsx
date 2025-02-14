fd4603d4a2a8afae387614cc75a8185d70b5cc30
KemalArasTosunlar <tosunlarkemal@gmail.com>
2025-02-14 18:05:40 +0300

    self-test-3

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import { Routes, Route } from "react-router-dom";
-import Header from './layout/Header';
-
-import Footer from './layout/Footer';
-import HomePage from './pages/HomePage';
-
+import { Routes, Route } from "react-router-dom"
+import Header from "./components/layout/Header"
+import Footer from "./components/layout/Footer"
+import HomePage from "./components/pages/HomePage"
 
 function App() {
   return (
     <>
-      <Header /> {/* This will be shown on all pages */}
-       <Routes>
+      <Header />
+      <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
-      <Footer /> {/* This will be shown on all pages */}
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
+
