179dc54d356bc635aa68dd0c6af72b5429656229
KemalArasTosunlar <tosunlarkemal@gmail.com>
2025-02-14 17:14:53 +0300

    refactor: simplify routing structure by removing nested routes in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,16 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
+import App from "./App.jsx";  
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import App from "./App.jsx";  
-import HomePage from "./pages/HomePage";  
 import "./index.css";
-import ShopPageDesktop from "./pages/ShopPageDesktop.jsx";
-import ContactPageDesktop from "./pages/ContactPageDesktop.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [
-      { path: "/", element: <HomePage /> },
-      { path: "shop", element: <ShopPageDesktop /> },
-      { path: "about", element: <div>About Page</div> },
-      { path: "blog", element: <div>Blog Page</div> }, 
-      { path: "contact", element: <ContactPageDesktop/> },
-    ],
   },
 ]);
 
