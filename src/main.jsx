495b3aaabc3200a7c506c11d9a7f1b378d4c47c6
KemalArasTosunlar <tosunlarkemal@gmail.com>
2025-02-14 18:41:51 +0300

    fix: wrap App component with BrowserRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import * as React from "react"
-import * as ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import App from "./App"
-import "./index.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App"; // Ensure App is correctly imported
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-   
-)
-
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
