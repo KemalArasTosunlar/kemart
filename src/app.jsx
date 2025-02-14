fdf2f97a9297a2ecd139562c48bd10b4d36105ed
KemalArasTosunlar <tosunlarkemal@gmail.com>
2025-02-14 18:04:36 +0300

    self-test2

--- src/app.jsx
+++ src/app.jsx
@@ -10,12 +10,9 @@ function App() {
   return (
     <>
       <Header /> {/* This will be shown on all pages */}
-      
-      <Routes>
+       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
-      
- 
       <Footer /> {/* This will be shown on all pages */}
     </>
   );
